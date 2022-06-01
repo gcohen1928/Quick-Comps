61f112392fd6330bb13df9483c540e78f1d3641f
michaelperl <mperl0914@gmail.com>
2022-06-01 12:19:36 +0200

    Changes to accept houses

--- Apartments-server/scanner.js
+++ Apartments-server/scanner.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-let NUMBER_OF_RESULTS = 25 // Can be less than 25
-let URL = 'https://www.apartments.com/arlington-tx-76010/'
+//let URL = 'https://www.apartments.com/arlington-tx-76010/'
+let URL = 'https://www.apartments.com/?sk=83fcdebd8994b96d97e6a51a03319369&bb=h5th__m10J7tzpoH'
 
 
 function link_to_fileName(link){
@@ -22,64 +22,10 @@ function link_to_fileName(link){
 	return tmp.concat(".csv");
 }
 
-function convert_date(date){
-	var months = {
-		Jan:1,
-		Feb:2,
-		Mar:3,
-		Apr:4,
-		May:5,
-		Jun:6,
-		Jul:7,
-		Aug:8,
-		Sep:9,
-		Oct:10,
-		Nov:11,
-		Dec:12
-	}
-	if(!(date === "Now")){
-		var day = months[date.substring(0,3)];
-		day = day.toString();
-		day = day.concat("/");
-		day = day.concat(date.slice(-2));
-		return day;
-
-	} else{
-		return date;
-	}
-}
 
 
 const link_to_csv = async (link) => {
-	let fileName = link_to_fileName(link);
-	fileName = "./apartment_csvs/".concat(fileName);
-	////////////////////////////// CSV DECLERATION ///////////////////////////////////
-
-	const createCSVWriter = require("csv-writer").createObjectCsvWriter;
-
-	const csvWriter = createCSVWriter({
-		
-		path: fileName,
-		header: [
-			{id: 'name', title: 'Property_Title'},
-			{id: 'state', title: 'State'},
-			{id: 'city', title: 'City'},
-			{id: 'zip', title: 'Zip'},
-		 	{id: 'addy', title: 'Address'},
-			{id: 'year', title: 'Year'},
-			{id: 'phone', title: 'Phone'},
-			{id: 'link', title: 'Link'},
-			{id: 'bed', title: 'Beds'},
-			{id: 'bath', title: 'Baths'},
-			{id: 'sf', title: 'SQFT'},
-			{id: 'rent', title: 'Rent'},
-			{id: 'psf', title: 'PSF'},
-			{id: 'num', title: 'APTCOUNT'},
-			{id: 'avail', title: 'Availabilty'}
-		]
-	});
 	let records = [];
-	////////////////////////////// CSV DECLERATION ///////////////////////////////////
 
 	////////////////////////////// SCRAPE MAIN PAGE //////////////////////////////////
 	let links = new Set(); //links for property pages to be scraped
@@ -109,9 +55,11 @@ const link_to_csv = async (link) => {
 			//// COMPLETION TRACKER ////
 
 			try{
+
 				const response = await axios.get(PROP_LINKS[i]);
 				const $ = cheerio.load(response.data);
 				
+				const propDesc = $("#breadcrumbs-container > span:nth-child(1) > a").attr('data-type');
 				const propName = $("#propertyName").text().trim();
 				const propAddy = $("#propertyAddressRow > div > h2 > span:nth-child(1)").text().trim();
 				const propCity = $("#propertyAddressRow > div > h2 > span:nth-child(2)").text().trim();
@@ -122,31 +70,22 @@ const link_to_csv = async (link) => {
 				propUnitCount = parseInt(propUnitCount.substring(0, propUnitCount.indexOf(' '))); 
 				const propPhone = $("#officeHoursSection > div > div > div:nth-child(1) > div.phoneNumber > a").text().trim();
 				const propLink = $("#officeHoursSection > div > div > div:nth-child(1) > div.mortar-wrapper > a").attr('href');
-				let apartments = [];
-				$('li[class ="unitContainer js-unitContainer "]').each(function () {
-					var tmp = [];
-					var beds = $(this).attr('data-beds');
-					var baths = $(this).attr('data-baths');
-					var price = $(this).find('div[class = "pricingColumn column"] span:nth-child(2)').text().trim().replace(/\D/g,'');
-					var sqft = $(this).find('div[class = "sqftColumn column"] span:nth-child(2)').text().trim().replace(/\D/g,'');
-					var avail = $(this).find('div[class = "availableColumn column"] span[class = "dateAvailable"]').text().trim();
-					avail = avail.substring(avail.indexOf('\n') + 1).trim();
-					var psf = Number(parseInt(price) / parseInt(sqft)).toFixed(2);
-					//avail = convert_date(avail);
-					if(price > 0){
-						tmp.push(parseInt(beds))
-						tmp.push(parseInt(baths));
-						tmp.push(parseInt(sqft));
-						tmp.push(parseInt(price));
-						tmp.push(psf);
-						tmp.push(avail);
-						apartments.push(tmp);
-					}
-				});
-		////////////////////////////// SCRAPE PROPERTY PAGES /////////////////////////////
 
-		////////////////////////////// WRITE TO CSV /////////////////////////////////////
-				for(let j = 0; j < apartments.length; j++){
+				if(propDesc == "houses"){
+					var beds = $('#pricingView > div > div > div > div > div.column1 > div > h4 > span:nth-child(1) > span:nth-child(1)').text().trim().replace(/\D/g,'');
+					var baths = $('#pricingView > div > div > div > div > div.column1 > div > h4 > span:nth-child(1) > span:nth-child(2)').text().trim().replace(/\D/g,'');
+					var rent = $('#pricingView > div > div > div > div > div.column1 > div > h3 > span.rentLabel').text().trim().replace(/\D/g,'');
+					var sqft = $('#priceBedBathAreaInfoWrapper > div > div > ul > li:nth-child(4) > div > p.rentInfoDetail').text().trim().replace(/\D/g,'');
+					sqft = parseInt(sqft);
+					var avail = $('#pricingView > div > div > div > div > div.column1 > div > h4 > span.detailsTextWrapper.leaseDepositLabel > span.availabilityInfo').text().trim();
+					if(sqft == NaN){
+						var psf = NaN;
+					} else{
+						var psf = Number(parseInt(rent) / sqft).toFixed(2);
+					}
+					if(avail == "Available Now"){
+						avail = "Now";
+					}
 					records.push({
 						name: propName,
 						state: propState,
@@ -157,33 +96,77 @@ const link_to_csv = async (link) => {
 						count: propUnitCount,
 						phone: propPhone,
 						link: propLink,
-						bed: apartments[j][0],
-						bath: apartments[j][1],
-						sf: apartments[j][2],
-						rent: apartments[j][3],
-						psf: apartments[j][4],
-						avail: apartments[j][5]
+						bed: parseInt(beds),
+						bath: parseInt(baths),
+						sf: parseInt(sqft),
+						rent: parseInt(rent),
+						psf: parseFloat(psf),
+						avail: avail,
+						notes: "Single Family Home"
 					});
+					
+				} else{
+					let apartments = [];
+					$('li[class ="unitContainer js-unitContainer "]').each(function () {
+						var tmp = [];
+						var beds = $(this).attr('data-beds');
+						var baths = $(this).attr('data-baths');
+						var rent = $(this).find('div[class = "pricingColumn column"] span:nth-child(2)').text().trim().replace(/\D/g,'');
+						var sqft = $(this).find('div[class = "sqftColumn column"] span:nth-child(2)').text().trim().replace(/\D/g,'');
+						var avail = $(this).find('div[class = "availableColumn column"] span[class = "dateAvailable"]').text().trim();
+						avail = avail.substring(avail.indexOf('\n') + 1).trim();
+						var psf = Number(parseInt(rent) / parseInt(sqft)).toFixed(2);
+						//avail = convert_date(avail);
+						if(rent > 0){
+							tmp.push(parseInt(beds))
+							tmp.push(parseInt(baths));
+							tmp.push(parseInt(sqft));
+							tmp.push(parseInt(rent));
+							tmp.push(parseInt(psf));
+							tmp.push(avail);
+							apartments.push(tmp);
+						}
+					});
+			////////////////////////////// SCRAPE PROPERTY PAGES /////////////////////////////
+
+			////////////////////////////// WRITE TO CSV /////////////////////////////////////
+					for(let j = 0; j < apartments.length; j++){
+						records.push({
+							name: propName,
+							state: propState,
+							zip: propZip,
+							city: propCity,
+							addy: propAddy,
+							year: propYear,
+							count: propUnitCount,
+							phone: propPhone,
+							link: propLink,
+							bed: apartments[j][0],
+							bath: apartments[j][1],
+							sf: apartments[j][2],
+							rent: apartments[j][3],
+							psf: apartments[j][4],
+							avail: apartments[j][5],
+							notes: "MF-APT"
+						});
+					}
 				}
-			} catch (error){
+				
+				} catch (error){
 				throw error;
 			}
-
-
-		}
+		}	
 
 	}catch (error){
 		throw error;
 	}
-	// csvWriter.writeRecords(records).then(() => {
-	// 	console.log("...Done");
-	// });
+
 	console.log(records)
 	return records
 
 	////////////////////////////// WRITE TO CSV /////////////////////////////////////
 }
 
-//link_to_csv(URL);
+link_to_csv(URL);
 
-module.exports = link_to_csv
+//module.exports = link_to_csv
