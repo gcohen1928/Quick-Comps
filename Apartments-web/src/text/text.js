b8abd4825df78bbbe40f22aeb746320e1c915185
gcohen1928 <gabriel.h.cohen@vanderbilt.edu>
2022-06-01 15:27:15 +0200

    Loading and rerun feature

--- Apartments-web/src/text/text.js
+++ Apartments-web/src/text/text.js
@@ -20,5 +20,4 @@ export const ERRORS = {
     204 : 'No properties available in this location. Please search another place',
     503 : 'Something went wrong. Make sure your location is in the United States. If not, our servers might be down so check in again shortly!',
     1 : 'It looks like your computer failed to reach our servers. Ensure you have a good internet connection and try again.'
-
 }
