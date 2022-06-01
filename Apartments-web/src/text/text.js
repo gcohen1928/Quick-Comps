d387a010d2e489cc78cc867100a73ad1d52ac913
michaelperl <mperl0914@gmail.com>
2022-06-01 12:53:49 +0200

    change csv

---
+++ Apartments-web/src/text/text.js
@@ -0,0 +1,25 @@
+export const CSVHEADER = [
+    { label: 'Property_Title', key: 'name' },
+    { label: 'State', key: 'state' },
+    { label: 'City', key: 'city' },
+    { label: 'Zip', key: 'zip' },
+    { label: 'Address', key: 'addy' },
+    { label: 'Year', key: 'year' },
+    { label: '#_Units', key: 'count' },
+    { label: 'Phone', key: 'phone' },
+    { label: 'Link', key: 'link' },
+    { label: 'Beds', key: 'bed' },
+    { label: 'Baths', key: 'bath' },
+    { label: 'SQFT', key: 'sf' },
+    { label: 'Rent', key: 'rent' },
+    { label: 'PSF', key: 'psf' },
+    { label: 'Availabilty', key: 'avail' },
+    { label: 'Notes', key: 'notes'}
+]
+
+export const ERRORS = {
+    204 : 'No properties available in this location. Please search another place',
+    503 : 'Something went wrong. Make sure your location is in the United States. If not, our servers might be down so check in again shortly!',
+    1 : 'It looks like your computer failed to reach our servers. Ensure you have a good internet connection and try again.'
+
+}
